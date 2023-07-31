c96e60268fe4abdb1ec76e694b0426455c335dc7
bigjosh112 <oladayoakinolaoreoluwa@example.com>
2023-08-01 00:31:04 +0100

    Tweets page

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,7 +1,12 @@
 import { View, Text, Image, StyleSheet } from "react-native";
+import { TweetType } from "@/types";
+import { Entypo, EvilIcons } from '@expo/vector-icons';
 
+type TweetProps = {
+    tweet: TweetType;
+}
 
-const Tweet = ({tweet}) => {
+const Tweet = ({tweet}: TweetProps) => {
     
     return(
         <View style={styles.container}>
@@ -9,8 +14,34 @@ const Tweet = ({tweet}) => {
             style ={styles.userImage}/>
                 
             <View style={styles.mainContainer}>
+                
+                <View style={{flexDirection: 'row'}}>
                 <Text style= {styles.name}>{tweet.user.name}</Text>
+                <Text style={styles.username}>{tweet.user.username}</Text>
+                <Text style={styles.username}>. 1h</Text>
+                <Entypo 
+                name="dots-three-horizontal" 
+                size={16} 
+                color="grey"
+                style={{marginLeft: 'auto'}} />
+                </View>
                 <Text style={styles.content}>{tweet.content}</Text>
+                
+
+                {/* optional impression */}
+               {tweet.image && <Image src={tweet.image} style={styles.image}/>}
+
+               <View style= {styles.footer}>
+                    {/* comment */}
+                    <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                        {/* ICONS */}
+                            <EvilIcons name="comment" size={22} color= 'grey' />
+                        {/* Number */}
+                        <Text style={{color: 'grey', fontSize: 12}}>12</Text>
+                    </View>
+
+                     
+               </View>
             </View>
       
     </View>
@@ -42,6 +73,22 @@ const styles = StyleSheet.create({
       marginTop: 5,
       lineHeight: 20,
       color: "white"
+    },
+    image: {
+        width: '100%',
+        aspectRatio: 16/9,
+        marginTop: 10,
+        borderRadius: 15,
+    },
+    username: {
+        color: 'grey',
+        marginLeft: 5
+
+    },
+    //footer
+    footer: {
+        flexDirection: 'row',
+        marginTop: 10
     }
     
   });
